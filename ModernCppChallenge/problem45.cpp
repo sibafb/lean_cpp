343297ed4aca9f904b22a7b786812738363db13e
Akiemon <siba16fb@gmail.com>
2023-01-07 08:51:11 +0900

    add problem45

--- ModernCppChallenge/problem45.cpp
+++ ModernCppChallenge/problem45.cpp
@@ -1,34 +1,64 @@
-template<typename T, typename F>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <assert.h>
+
+template <class T, class Compare = std::less<typename std::vector<T>::value_type>>
 class priority_queue
 {
-    priority_queue(typename F)
-    
+    typedef typename std::vector<T>::value_type value_type;
+    typedef typename std::vector<T>::size_type size_type;
+    typedef typename std::vector<T>::size_type reference;
+    typedef typename std::vector<T>::size_type const_reference;
+public:
+    bool empty() const noexcept { return data.empty(); }
+    size_type size() const noexcept { return data.size(); }
+
+    void push(value_type const & value)
     {
-        data.clear();
+        data.push_back(value);
+        std::push_heap(std::begin(data), std::end(data), comparer);
     }
 
-    void push(const T & a)
-    {
-        data.push_back(a);
-    }
     void pop()
     {
-        data.pop();
+        std::pop_heap(std::begin(data), std::end(data), comparer);
+        data.pop_back();
     }
-    const T& top()
-    {
-        return data.end();
-    }
-    unsigned int& size()
-    {
-        return data.size();
-    }
-    bool empty()
-    [
-        return 
-    ]
 
+    const_reference top() const { return data.front(); }
+    void swap(priority_queue& other) noexcept
+    {
+        swap(data, other.data);
+        swap(comparer, other.comparer);
+    }
 private:
-    std::deque<T> data;
-    F& compare(const T & a, const T & b);
+    std::vector<T> data;
+    Compare comparer;
+};
+
+template <class T, class Compare>
+void swap(priority_queue<T, Compare>& lhs,
+          priority_queue<T, Compare>& rhs)
+          noexcept(noexcept(lhs.swap(rhs)))
+{
+    lhs.swap(rhs);
+}
+
+int main()
+{
+    priority_queue<int> q;
+    for ( int i : {1, 5, 3, 1, 13, 21, 8})
+    {
+        q.push(i);
+    }
+
+    assert(!q.empty());
+    assert(q.size() == 7);
+
+    while(!q.empty())
+    {
+        std::cout << q.top() << ' ';
+        q.pop();
+    }
 }
