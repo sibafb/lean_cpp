79f78df85bcd99e23b9716e11a7633f2045ae311
Akiemon <siba16fb@gmail.com>
2023-01-13 11:33:08 +0900

    add problem85

--- ModernCppChallenge/problem85.cpp
+++ ModernCppChallenge/problem85.cpp
@@ -1,4 +1,10 @@
- 
+#include <iostream>
+#include <vector>
+
+#include "sqlite3.h"
+#include "sqlite_modern_cpp.h"
+//#include "movies.h"
+
 struct casting_role
 {
     std::string actor;
@@ -13,7 +19,9 @@ struct movie
     std::vector<casting_role> cast;
     std::vector<std::string> directors;
     std::vector<std::string> writers;
-}
+};
+
+using movie_list = std::vector<movie>;
 
 void print_movie(movie const & m)
 {
@@ -27,14 +35,29 @@ void print_movie(movie const & m)
     for (auto const & w : m.writers) std::cout << w << ",";
     std::cout << std::endl;
     std::cout << " cast: ";
-    for (auto cons & r : m.cast)
+    for (auto const & r : m.cast)
         std::cout << r.actor << " (" << r.role << "), ";
     std::cout << std::endl << std::endl;
 }
 
-std::vector<std::string> get_writers(sqlite3_int64 const movie_id, sqlite::datebase & db)
+std::vector<std::string> get_directors(sqlite3_int64 const movie_id, sqlite::database & db)
 {
-    std::vector<<std::string> result;
+    std::vector<std::string> result;
+    db << R"(select p.name from writers as w
+            join persons as p on w.personid = p.rowid
+            where w.movieid = ?;)"
+        << movie_id
+        >> [&result](std::string const name)
+        {
+            result.emplace_back(name);
+        };
+
+    return result;
+}
+
+std::vector<std::string> get_writers(sqlite3_int64 const movie_id, sqlite::database & db)
+{
+    std::vector<std::string> result;
     db << R"(select p.name form writers as w
             join persons as p on w.personid = p.rowid
             where w.movieid = ?;)"
@@ -42,46 +65,47 @@ std::vector<std::string> get_writers(sqlite3_int64 const movie_id, sqlite::dateb
         >> [&result](std::string const name)
         {
             result.emplace_back(name);
-        }
-
-    return result;
-}
-
-std::vector<casting_role> get_cast(sqlite3_int64 const movie_id, sqlite::database & db)
-{
-    std::vector<casting_role> result;
-    db << R"(select p.name, c.role form casting as c
-            koin persons as p on c. personid = p.rowid
-            where c.movieid = ?;)"
-        << movie_id
-        >> [&result](std::string const name, std::string role)
-        {
-            result.emplace_back(casting_role{ name, role }};)
         };
 
     return result;
 }
 
+std::vector<casting_role> get_cast(sqlite3_int64 const movie_id, 
+                                   sqlite::database & db)
+{
+   std::vector<casting_role> result;
+   db << R"(select p.name, c.role from casting as c
+         join persons as p on c.personid = p.rowid
+         where c.movieid = ?;)"
+      << movie_id
+      >> [&result](std::string const name, std::string role)
+   {
+      result.emplace_back(casting_role{ name, role });
+   };
+
+   return result;
+}
+
 movie_list get_movies(sqlite::database & db)
 {
-    movie_list movies;
-    
-    db << R"(select rowid, * from movies;)"
-        >> [&movies, &db](sqlite3_int64 const rowid, std::string const & title, int const year, int const length)
-        {
-            movies.emplace_back(
-                movie{
-                    static_cast<unsigned int>(rowid),
-                    title,
-                    year,
-                    static_cast<unsigned int>(length),
-                    get_cast(rowid, db),
-                    get_directors(rowid, db),
-                    get_writers(rowid, db)
-            });
-        }
+   movie_list movies;
 
-    return movies;
+   db << R"(select rowid, * from movies;)"
+      >> [&movies, &db](sqlite3_int64 const rowid, std::string const & title, 
+                        int const year, int const length)
+   {
+      movies.emplace_back(movie{
+         static_cast<unsigned int>(rowid),
+         title,
+         year,
+         static_cast<unsigned int>(length),
+         get_cast(rowid, db),
+         get_directors(rowid, db),
+         get_directors(rowid, db)
+         });
+   };
+
+   return movies;
 }
 
 int main()
@@ -90,7 +114,9 @@ int main()
     {
         sqlite::database db(R"(cppchallenger85.db)");
 
+        std::cout << "dbopend" <<std::endl;
         auto const movies = get_movies(db);
+        std::cout << "got movies" <<std::endl;
         for (auto const & m : movies)
             print_movie(m);
     }
