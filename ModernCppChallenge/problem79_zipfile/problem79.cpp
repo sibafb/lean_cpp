f4d7e0b1ea0f6fa2660c5896bed66e99dd7a2724
Akiemon <siba16fb@gmail.com>
2023-05-03 00:26:52 +0900

    Add problem79

---
+++ ModernCppChallenge/problem79_zipfile/problem79.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <string>
+#include <string_view>
+#include <vector>
+#include <regex>
+#include <filesystem>
+
+#include "ZipFile.h"
+#include "ZipArchive.h"
+
+namespace fs = std::filesystem;
+
+std::vector<std::string> find_in_archive(
+    fs::path const & archivepath,
+    std::string_view pattern)
+{
+    std::vector<std::string> results;
+
+    if (fs::exists(archivepath))
+    {
+        try
+        {
+            auto archive = ZipFile::Open(archivepath.string());
+
+            for (size_t i = 0; i< archive->GetEntriesCount(); ++i)
+            {
+                auto entry = archive->GetEntry(i);
+                if (entry)
+                {
+                    if (!entry->IsDirectory())
+                    {
+                        auto name = entry->GetName();
+                        if (std::regex_match(name, std::regex{ pattern.data() }))
+                        {
+                            results.push_back(entry->GetFullName());
+                        }
+                    }
+                }
+            }
+        }
+        catch(std::exception const & e)
+        {
+            std::cerr << e.what() << std::endl;
+        }
+    }
+
+    return results;
+}
+
+int main()
+{
+    std::string archivepath;
+    std::cout << "Archive path:";
+    std::cin >> archivepath;
+
+    std::string pattern;
+    std::cout << "Pattern:";
+    std::cin >> pattern;
+
+    std::cout << "Results:" << std::endl;
+
+    for (auto const & name : find_in_archive(archivepath, pattern))
+    {
+        std::cout << name << std::endl;
+    }
+}
